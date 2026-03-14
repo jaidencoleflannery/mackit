041172f6c6e305fa16081c6174e7405ad6aed71c
Cole Flannery <jaidencoleflannery@gmail.com>
2026-03-14 14:36:06 -0500

    quickstart | fixed pathing.

--- auto-configuration.sh
+++ auto-configuration.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/modules/preferences.sh" true
+SCRIPT_DIR="${0:A:h}"
 source "$SCRIPT_DIR/modules/vscode-keybindings.sh" true
 source "$SCRIPT_DIR/modules/vscode-settings.sh" true
+source "$SCRIPT_DIR/modules/preferences.sh" true
 source "$SCRIPT_DIR/modules/packages.sh" true
 
 echo -e "\n[ Mackit ]\n~ This script optimized your MacOS configuration for efficiency.\n~ If you found this useful, please leave a star on the project: https://github.com/jaidencoleflannery/mackit \n"
